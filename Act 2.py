bcf2c241b4887232244293bbec8fc33052d74ed0
Mystic-78902 <charani.avula@gmail.com>
2025-02-15 17:08:06 +0530

    Python

---
+++ Act 2.py
@@ -0,0 +1,6 @@
+print("Welcome to the world of programs")
+print(4)
+print("hello Python \ n")
+print("the Keywords in python are:")
+import keyword
+print(keyword.kwlist)
---
+++ Main.py
@@ -0,0 +1,6 @@
+# this it a comment
+print("hello my name is mike")
+print("I am 6 years old ")
+print("I love to eat strawberry and pecan ice cream!")
+# add 2 numbers mike
+print("the answer to 4+9 is:", 4 + 9)
